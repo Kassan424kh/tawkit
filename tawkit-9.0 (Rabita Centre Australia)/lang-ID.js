2013cbfab4b1c10c6193eca4ef7b64bf909e50b5
Khalil Khalil <k.khalil@satzmedia.de>
2022-10-22 22:16:27 +0200

    added Rabita Centre Australia

---
+++ tawkit-9.0 (Rabita Centre Australia)/lang-ID.js
@@ -0,0 +1,116 @@
+JS_eLang =
+{
+
+	HereMosqueName		: "Nama Masjid",
+	HereMosqueMessage	: "Pengumuman untuk Jemaat.",
+	                                                                                                                                                                                      
+	MinutesToIqama		: "Menit Menuju Iqama",
+	MinutesToDoha		: "Menit ke Doha",
+	AzanCalling			: "Azan",
+	NextPrayTime	 	: "sholat berikutnya",
+	NamesOfPrayings		: "FAJR, SUNRISE, DZUHUR, ASHAR, MAGHRIB, ISYA",
+	NamesWeekDays		: "Minggu,Senin,Selasa,Rabu,Kamis,Jumat,Sabtu",
+	NamesMonthsMiladi	: "Januari, Februari, Maret, April, Mei, Juni, Juli, Agustus, September, Oktober, November, Desember",
+	MenuMonthsHijri		: "Muharram, Safar, Rabi' al-Awwal, Rabi' al-Akhir, Jumada al-Awwal, Jumada al-Akhirah, Rajab, Syaaban, Ramadhan, Syawal, Dhul-Qa'dah, Dhul-Hijjah.",
+	                                                                                                                                                                                      
+	MenuFullScreen		: "Layar penuh",
+	MenuMosqueNAME		: "Edit Nama Masjid",
+	MenuMosqueMessage	: "Edit Pengumuman Masjid",
+	MenuLocation		: "Lokasi Masjid",
+	MenuHijriClick		: "Sesuaikan Tanggal Hijriah",
+	MenuThemes			: "Ubah Latar Belakang Layar",
+	MenuOptions			: "Pilihan",
+	MenuAzkar			: "Tampilan Azkar",
+	MenuSliders			: "Tampilan Slide",
+	MenuAdjustments		: "Menyesuaikan Waktu Sholat",
+	MenuBlackscreen		: "Menyesuaikan Layar Hitam",
+	MenuDimmerDirect	: "Sembunyikan Layar Manual",
+	MenuMosquelogo		: "Logo Masjid",
+	MenuRestartApp		: "Mulai Ulang Aplikasi",
+	MenuGoMobile		: "Aplikasi Seluler",
+	MenuAboutTawkit		: "Tentang Tawkit",
+                                                                                                                                                                                          
+	XXDoaAzanIqama		: "الدعاء بين الأذان والإقامة لا يرد",
+	XXFastingMonday		: "Reminder : Jangan Lupa Puasa Besok Senin",
+	XXFastingThursday	: "Reminder : Jangan Lupa Puasa Besok Kamis",
+	                                                                                                                                                                                      
+	XX_Themes_0			: "Klik gambar untuk menggunakan latar belakang pribadi",
+	XX_Themes_1			: "Untuk mengubah gambar latar belakang, ubah file",
+	XX_Themes_2			: "dalam folder",
+	XX_Themes_3			: "Untuk mengubah warna, klik teks di bawah",
+	XX_Themes_4			: "Edit Warna",
+	                                                                                                                                                                                      
+	XX_OPTION_0			: "Aktifkan 24 Jam",
+	XX_OPTION_1			: "Aktifkan Jam Penuh",
+	XX_OPTION_2			: "Redup Sholat Sebelumnya",
+	                                                                                                                                                                                      
+	XX_ADJUST_0			: "Waktu Antara Azan Dan Hitung Mundur",
+	XX_ADJUST_1			: "Tampilkan Hitung Mundur",
+	XX_ADJUST_2			: "Gunakan Suara Untuk Azan Dan Iqama",
+	XX_ADJUST_3			: "Tambahkan 30 Menit Ke Isya Di Bulan Ramadhan",
+	                                                                                                                                                                                      
+	XX_IQAMA			: "Iqamah",
+	XX_DOHA				: "Doha",
+	                                                                                                                                                                                      
+	XX_DIMMER_0			: "Aktifkan Layar Hitam Saat Berdoa",
+	XX_DIMMER_1			: "Layar Hitam 35 Menit Setelah Matahari Terbit",
+	XX_DIMMER_2			: "Layar Hitam 50 Menit Setelah Isya",
+	                                                                                                                                                                                      
+	XX_DIMMER_3			: "Layar Hitam Sebelum Dohr",
+	XX_DIMMER_4			: "Tampilkan Layar Setelah Dohr",
+	XX_DIMMER_5			: "Durasi Sholat Untuk Menyembunyikan Layar",
+	                                                                                                                                                                                      
+	XX_AZKAR_0			: "Aktifkan Gambar Azkar Setelah Sholat",
+	XX_AZKAR_1			: "Gambar Ditampilkan/Disembunyikan Setiap 20 Detik",
+	XX_AZKAR_2			: "Gambar Azkar Ada Di Folder : ",
+	XX_AZKAR_3			: "Pilih Durasi Munculnya Azkar Setelah Sholat :",
+	XX_AZKAR_4			: "Tampilkan Hanya Satu Gambar, Tetap Selama 5 Menit",
+	XX_AZKAR_5			: "Untuk Menguji Azkar, Klik Waktu Dohr",
+	XX_AZKAR_6			: "Untuk Menghentikan Azkar, Klik Pada Layar Azkar.",
+	                                                                                                                                                                                      
+	XX_MOBILE_1			: "Untuk Membuat Aplikasi Seluler Masjid Anda, Buka : ",
+	                                                                                                                                                                                      
+	XX_SLIDERS_0		: "Aktifkan Slide Pribadi",
+	XX_SLIDERS_1		: "Slide adalah gambar yang ditampilkan secara acak di layar setelah Azkar",
+	XX_SLIDERS_2		: "Ditampilkan/disembunyikan setiap 20 detik hingga Sholat berikutnya",
+	XX_SLIDERS_3		: "Untuk menambahkan Slide Anda sendiri, tempatkan hingga 7 gambar di folder (Slide).",
+	XX_SLIDERS_4		: "Untuk menguji Slide, klik Dohr",
+	XX_SLIDERS_5		: "Untuk menghentikan Slide, klik pada Layar Slide.",
+	                                                                                                                                                                                      
+	XX_TIMES_0			: "Gunakan Perhitungan",
+	XX_TIMES_1			: "Gunakan File",
+	XX_TIMES_2			: "Pilih Kota Masjid Anda",
+	XX_TIMES_3			: "Jika kota Anda tidak ada dalam daftar, pilih (00.000.PERSONAL)",
+	XX_TIMES_4			: "Dan ubah file (/data/wtimes-00.000.ini) agar sesuai dengan waktu Masjid Anda",
+	XX_TIMES_5			: "Muat Ulang Aplikasi",
+	                                                                                                                                                                                      
+	XX_TIMES_6			: "Sunting",
+	                                                                                                                                                                                      
+	XX_CALC_0			: "Negara:",
+	XX_CALC_1			: "Kota:",
+	XX_CALC_2			: "Metode:",
+	XX_CALC_3			: "Perhitungan Ashar:",
+	                                                                                                                                                                                      
+	XX_BOX_0			: "Klik tanggal Hijriah untuk menyesuaikan tanggal + 2 / -2",
+	XX_BOX_1			: "Untuk memasang logo Masjid anda pada aplikasi",
+	XX_BOX_2			: "Ubah file (logo.png) di folder aplikasi.",
+	                                                                                                                                                                                      
+	XX_COMMANDER_0		: "Aktifkan Penjadwal Perintah Suara",
+	XX_MISC_0			: "RELOAD HALAMAN SETELAH MODIFIKASI",
+	XX_MISC_1			: "KLIK FAJR UNTUK MENGUBAH LATAR BELAKANG",
+	
+	XX_NO_SELL			: "PERANGKAT LUNAK GRATIS, DILARANG MENJUALNYA",
+	
+	XX_JOMOA			: "JUMAT",
+	XX_IQAMAASHOUR		: "Show Iqama with hour",
+	XX_SUMMERTIME		: "Tambahkan otomatis 1 jam ke waktu sholat di musim panas",
+
+	
+	W9_ARABICDIGITS			: "Enable Arabic Digits",
+	XX_WCSV_MESSAGE			: "Enable own file (wcsv.ini) for praying Times and Iqama",
+	W9_SetJOMOA_TIME		: "Adjust Jumuah time",
+	W9_eIQAMAUNDERNAMES		: "Put Iqama times under the Prayers names",
+	W9_setFridayJomoaTIME	: "Jomoa time of your Mosque. If automatic every Friday, write AUTO",
+
+
+};
