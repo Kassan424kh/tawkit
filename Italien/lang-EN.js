de00dde33e37246cf128b0a05c0e0644329b15fd
Khalil Khalil <k.khalil@satzmedia.de>
2022-11-27 13:30:57 +0100

    added Italien Mashged

---
+++ Italien/lang-EN.js
@@ -0,0 +1,115 @@
+JS_eLang = 
+{
+	
+	HereMosqueName		: "Mosque Name",
+	HereMosqueMessage	: "...",
+	
+	MinutesToIqama		: "Minutes to Iqama",
+	MinutesToDoha		: "Minutes to Doha ",
+	AzanCalling			: " Azan ",
+	NextPrayTime	 	: "Remaining time | to next prayer",
+	NamesOfPrayings		: "FAJR,SUNRISE,DOHR,ASR,MAGHRIB,ISHA",
+	NamesWeekDays		: "Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday",
+	NamesMonthsMiladi	: "January, February, March, April, May, June, July, August, September, October, November, December",
+	MenuMonthsHijri		: "Muharram, Safar, Rabi’ al-Awwal, Rabi’ al-Akhir, Jumada al-Awwal, Jumada al-Akhirah, Rajab, Shaaban, Ramadan, Shawwal, Dhul-Qa’dah, Dhul-Hijjah.",
+	
+	MenuFullScreen		: "Full Screen",
+	MenuMosqueNAME		: "Edit Mosque Name",
+	MenuMosqueMessage	: "Edit Mosque Announcement",
+	MenuLocation		: "Mosque Location",
+	MenuHijriClick		: "Adjust Hijri Date",
+	MenuThemes			: "Change Screen Background",
+	MenuOptions			: "Options",
+	MenuAzkar			: "Azkar Display",
+	MenuSliders			: "Slides Display",
+	MenuAdjustments		: "Adjust Prayers Times",
+	MenuBlackscreen		: "Adjust Black-Screen",
+	MenuDimmerDirect	: "Hide Screen Manualy",
+	MenuMosquelogo		: "Mosque Logo",
+	MenuRestartApp		: "Restart The Application",
+	MenuGoMobile		: "Mobile Application",
+	MenuAboutTawkit		: "About Tawkit",
+
+	XXDoaAzanIqama		: "الدعاء بين الأذان والإقامة لا يرد",
+	XXFastingMonday		: "Reminder : Don't Forget to Fast Tomorrow Monday",
+	XXFastingThursday	: "Reminder : Don't Forget to Fast Tomorrow Thursday",
+	
+	XX_Themes_0			: "Click on picture to use personal background",
+	XX_Themes_1			: "To change the background picture, modify the file",
+	XX_Themes_2			: "in folder",
+	XX_Themes_3			: "To change colors, click on text below",
+	XX_Themes_4			: "Edit Colors",
+	
+	XX_OPTION_0			: "Enable 24 Hours",
+	XX_OPTION_1			: "Enable Full Clock",
+	XX_OPTION_2			: "Dimm Past Prayers",
+	
+	XX_ADJUST_0			: "Time Between Azan And The Countdown",	
+	XX_ADJUST_1			: "Show The Countdown",
+	XX_ADJUST_2			: "Use Voice For Azan And Iqama",
+	XX_ADJUST_3			: "Add 30 Minutes To Isha In Ramadan",
+	
+	XX_IQAMA			: "Iqama",
+	XX_DOHA				: "Doha",
+	
+	XX_DIMMER_0			: "Activate Black-Screen while Praying",
+	XX_DIMMER_1			: "Black-Screen 35 Minutes After Sunrize",
+	XX_DIMMER_2			: "Black-Screen 50 Minutes After Isha",
+	
+	XX_DIMMER_3			: "Black-Screen Before Dohr",
+	XX_DIMMER_4			: "Show Screen After Dohr",
+	XX_DIMMER_5			: "Prayers Durations To Hide Screen",
+	
+	XX_AZKAR_0			: "Activate Azkar Pictures After Prayers",
+	XX_AZKAR_1			: "Pictures Are Shown/Hidden Every 20 Seconds",
+	XX_AZKAR_2			: "Azkar Pictures Are In Folder : ",
+	XX_AZKAR_3			: "Choose The Duration Of Azkar Appearing After The Prayer :",
+	XX_AZKAR_4			: "Show Only One Image, Fixed For 5 Minutes",
+	XX_AZKAR_5			: "To Test Azkar, Click On The Dohr Time",
+	XX_AZKAR_6			: "To Stop Azkar, Click On The Azkar Screen.",
+	
+	XX_MOBILE_1			: "To Create A Mobile App For Your Mosque, Go To : ",
+	
+	XX_SLIDERS_0		: "Activate Personal Slides",
+	XX_SLIDERS_1		: "Slides are images that are randomly displayed on the screen after Azkar",
+	XX_SLIDERS_2		: "They are shown/hidden every 20 seconds until the next Prayer",
+	XX_SLIDERS_3		: "To add your own Slides, place up to 7 images in the (Slides) folder.",
+	XX_SLIDERS_4		: "To test the Slides, click on Dohr",
+	XX_SLIDERS_5		: "To stop the Slides, click on Slides Screen.",
+	
+	XX_TIMES_0			: "Use Calculation",
+	XX_TIMES_1			: "Use Files",
+	XX_TIMES_2			: "Choose City Of Your Mosque",
+	XX_TIMES_3			: "If your city is not in the list, select (00.000.PERSONAL)",
+	XX_TIMES_4			: "And modify the file (/data/wtimes-00.000.ini) to suit the times of your Mosque",
+	XX_TIMES_5			: "Reload The Application",
+	
+	XX_TIMES_6			: "Edit",
+	
+	XX_CALC_0			: "Country : ",
+	XX_CALC_1			: "City : ",
+	XX_CALC_2			: "Method : ",
+	XX_CALC_3			: "Asr Calculation : ",
+	
+	XX_BOX_0			: "Click on the Hijri date to adjust the date + 2 / -2",
+	XX_BOX_1			: "To put your Mosque logo on the application, ",
+	XX_BOX_2			: "Modify the file (logo.png) in the application folder. ",
+	
+	XX_COMMANDER_0		: "Activate The Voice Commands Scheduler",
+	XX_MISC_0			: "RELOAD THE PAGE AFTER MODIFICATIONS !",
+	XX_MISC_1			: "CLICK ON FAJR TO SWITCH BETWEEN THEMES",
+	
+	XX_NO_SELL			: "FREEWARE APPLICATION, FORBIDDEN TO SELL IT",
+	
+	
+	XX_JOMOA			: "JUMUAH",
+	XX_IQAMAASHOUR		: "Show Iqama with hour",
+	XX_SUMMERTIME		: "Auto add 1 hour to praying times in summer",
+	
+	W9_ARABICDIGITS			: "Enable Arabic Digits",
+	XX_WCSV_MESSAGE			: "Enable own file (wcsv.ini) for praying Times and Iqama",
+	W9_SetJOMOA_TIME		: "Adjust Jumuah time",
+	W9_eIQAMAUNDERNAMES		: "Put Iqama times under the Prayers names",
+	W9_setFridayJomoaTIME	: "Jomoa time of your Mosque. If automatic every Friday, write AUTO",
+
+};
---
+++ Italien/tawkit-9.0/lang-FR.js
@@ -0,0 +1,116 @@
+JS_eLang = 
+{
+	
+	HereMosqueName 			: "Nom de la Mosquée",
+	HereMosqueMessage 		: "...",
+	
+	MinutesToIqama 			: "Minutes pour Iqama",
+	MinutesToDoha 			: "Minutes à Doha",
+	AzanCalling 			: "Azan",
+	NextPrayTime 			: "Prochaine Prière",
+	NamesOfPrayings 		: "FAJR,CHOROQ,DOHR,ASR,MAGHRIB,ISHA",
+	NamesWeekDays 			: "Dimanche,Lundi,Mardi,Mercredi,Jeudi,Vendredi,Samedi",
+	NamesMonthsMiladi 		: "Janvier, Février, Mars, Avril, Mai, Juin, Juillet, Août, Septembre, Octobre, Novembre, Décembre",
+	MenuMonthsHijri 		: "Muharram, Safar, Rabi' al-Awwal, Rabi' al-Akhir, Jumada al-Awwal, Jumada al-Akhirah, Rajab, Shaaban, Ramadan, Shawwal, Dhul-Qa'dah, Dhul-Hijjah",
+	
+	MenuFullScreen 			: "Plein écran",
+	MenuMosqueNAME 			: "Editer le nom de la Mosquée",
+	MenuMosqueMessage 		: "Editer l'annonce de la Mosquée",
+	MenuLocation 			: "Emplacement de la Mosquée",
+	MenuHijriClick 			: "Ajuster la date Hijri",
+	MenuThemes 				: "Changer le fond d'écran",
+	MenuOptions 			: "Options",
+	MenuAzkar 				: "Affichage d'Azkar",
+	MenuSliders 			: "Affichage des diapositives",
+	MenuAdjustments 		: "Ajuster les Prières",
+	MenuBlackscreen 		: "Ajuster l'écran noir",
+	MenuDimmerDirect 		: "Masquer l'écran manuellement",
+	MenuMosquelogo 			: "Logo de Mosquée ",
+	MenuRestartApp 			: "Redémarrer l'application",
+	MenuGoMobile 			: "Application mobile",
+	MenuAboutTawkit 		: "A propos de Tawkit",
+
+	XXDoaAzanIqama 			: "الدعاء بين الأذان والإقامة لا يرد",
+	XXFastingMonday 		: "Rappel : N'oubliez pas de jeûner demain Lundi",
+	XXFastingThursday 		: "Rappel : N'oubliez pas de jeûner demain Jeudi",
+	
+	XX_Themes_0 			: "Cliquez sur l'image pour utiliser votre fond d'écran personnel",
+	XX_Themes_1 			: "Pour changer l'image de fond, modifiez le fichier",
+	XX_Thémes_2 			: "dans le dossier",
+	XX_Themes_3 			: "Pour changer les couleurs, cliquez sur le texte ci-dessous",
+	XX_Thémes_4 			: "Modifier les couleurs",
+	
+	XX_OPTION_0 			: "Activer les 24 heures",
+	XX_OPTION_1 			: "Activer l'horloge complète",
+	XX_OPTION_2 			: "Réduire la lumière des prières passées",
+	
+	XX_ADJUST_0 			: "Temps entre Azan et le décompte",	
+	XX_ADJUST_1 			: "Afficher le compte à rebours",
+	XX_ADJUST_2 			: "Utiliser la voix pour l'Azan et l'Iqama",
+	XX_ADJUST_3 			: "Ajouter 30 minutes à Isha pendant le Ramadan",
+	
+	XX_IQAMA 				: "Iqama",
+	XX_DOHA 				: "Doha",
+	
+	XX_DIMMER_0 			: "Activer l'écran noir pendant la prière",
+	XX_DIMMER_1 			: "Écran noir 35 minutes après Chorouq",
+	XX_DIMMER_2 			: "Écran noir 50 minutes après Isha",
+	
+	XX_DIMMER_3 			: "Ecran noir avant Dohr",
+	XX_DIMMER_4 			: "Écran noir après Dohr",
+	XX_DIMMER_5 			: "Durée des prières pour cacher l'écran",
+	
+	XX_AZKAR_0 				: "Activer les images Azkar après les prières",
+	XX_AZKAR_1 				: "Les images sont affichées/cachées toutes les 20 secondes",
+	XX_AZKAR_2 				: "Les images d'Azkar sont dans le dossier : ",
+	XX_AZKAR_3 				: "Choisissez la durée d'apparition d'Azkar après la prière :",
+	XX_AZKAR_4 				: "Afficher une seule image, fixée pour 5 minutes",
+	XX_AZKAR_5 				: "Pour tester l'Azkar, cliquez sur le temps de Dohr",
+	XX_AZKAR_6 				: "Pour arrêter l'Azkar, cliquez sur l'écran d'Azkar",
+	
+	XX_MOBILE_1 			: "Pour créer une application mobile pour votre Mosquée, allez à : ",
+	
+	XX_SLIDERS_0 			: "Activer les diapositives personnelles",
+	XX_SLIDERS_1 			: "Les diapositives sont des images qui sont affichées aléatoirement sur l'écran après l'Azkar",
+	XX_SLIDERS_2 			: "Elles sont affichées/cachées toutes les 20 secondes jusqu'à la prochaine Prière",
+	XX_SLIDERS_3 			: "Pour ajouter vos propres diapositives, placez jusqu'à 7 images dans le dossier (Slides)",
+	XX_SLIDERS_4 			: "Pour tester les diapositives, cliquez sur Dohr",
+	XX_SLIDERS_5 			: "Pour arrêter les diapositives, cliquez sur lécran",
+	
+	XX_TIMES_0 				: "Utiliser le calcul",
+	XX_TIMES_1 				: "Utiliser les fichiers",
+	XX_TIMES_2 				: "Choisissez la ville de votre Mosquée",
+	XX_TIMES_3 				: "Si votre ville n'est pas dans la liste, sélectionnez (00.000.PERSONAL)",
+	XX_TIMES_4 				: "Et modifiez le fichier (/data/wtimes-00.000.ini) pour adapter les horaires de votre Mosquée",
+	XX_TIMES_5 				: "Rechargez l'application",
+	
+	XX_TIMES_6 				: "Editer",
+	
+	XX_CALC_0 				: "Pays : ",
+	XX_CALC_1 				: "Ville : ",
+	XX_CALC_2 				: "Méthode : ",
+	XX_CALC_3 				: "Asr : ",
+	
+	XX_BOX_0 				: "Cliquez sur la date Hijri pour ajuster la date + 2 / -2",
+	XX_BOX_1 				: "Pour mettre le logo de votre Mosquée sur l'application, ",
+	XX_BOX_2 				: "Modifiez le fichier (logo.png) dans le dossier de l'application. ",
+	
+	XX_COMMANDER_0			: "Activer le planificateur de commandes vocales",
+	XX_MISC_0				: "RECHARGER LA PAGE APRES MODIFICATIONS",
+	XX_MISC_1				: "CLIQUER SUR FAJR POUR CHANGER DE THÈME",
+	
+	XX_NO_SELL				: "LOGICIEL GRATUIT, INTERDIT DE LE VENDRE",
+
+	
+	XX_JOMOA				: "JOUMOA",
+	XX_IQAMAASHOUR			: "Afficher Iqama avec l'heure ",
+	XX_SUMMERTIME			: "Ajouter automatiquement 1 heure aux heures de prière en été",
+
+	W9_ARABICDIGITS			: "Activer les chiffres arabes",
+	XX_WCSV_MESSAGE			: "Activer le fichier personalisé (wcsv.ini) pour les heures de prières et Iqama",
+	W9_SetJOMOA_TIME		: "Fixer l'heure du Jumua",
+	W9_eIQAMAUNDERNAMES		: "Mettre les Iqama sous les noms des prières",
+	W9_setFridayJomoaTIME	: "Heure du Jomoa de votre mosquée. Si automatique tous les vendredis, écrivez AUTO",
+
+
+} ;
