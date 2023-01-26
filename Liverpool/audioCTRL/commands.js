b1ec2bbf43043d3e5114bad9194aa695e8ac3403
Khalil Khalil <46401401+Kassan424kh@users.noreply.github.com>
2023-01-26 20:08:52 +0000

    added Liverpool Gamee

---
+++ Liverpool/audioCTRL/commands.js
@@ -0,0 +1,62 @@
+/*
+
+TAWKIT COMMANDS FILE to control Mosque Lights and Audio via voice commands Before and After each Prayer
+
+Documentaion :
+
+. Days Names To Use 	: SUN/MON/TUE/WED/THU/FRI/SAT
+. Prayers Names To Use	: FAJR, SUNRISE, DOHR, ASR, MAGHRIB, ISHA, MIDNIGHT
+. Events to Use			: AFTER, BEFORE
+
+4 Actions to use : 
+
+		. MosqueON
+		. MosqueOFF
+		. AudioON
+		. AudioOFF
+
+
+
+Example of commands code :
+
+----- A Command To Switch On Mosque Lights 60Min BEFORE FAJR, During All Days Of The Week :
+60.BEFORE.FAJR.PLAY(MosqueON).SUN/MON/TUE/WED/THU/FRI/SAT
+
+
+----- A Command To Switch Off Mosque Lights 90 AFTER FAJR, During All Days Of The Week :
+90.AFTER.FAJR.PLAY(MosqueOFF).SUN/MON/TUE/WED/THU/FRI/SAT
+
+
+----- A Command To Switch On Mosque Audio on 3 Minutes BEFORE DOHR, Only on Friday:
+3.BEFORE.DOHR.PLAY(AudioON).FRI
+
+
+
+Replace The xxxxxxxxxxxxxxxxx Lines With Your Commands ! 
+You Can Add Many As Needed.
+
+----------------------------------------------------------------------------------------------------
+*/
+
+
+var JS_COMMANDS_LISTING = 
+[
+
+"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Liverpool/lang-TR.js
@@ -0,0 +1,115 @@
+JS_eLang = 
+{
+	
+	HereMosqueName		: "Cami Adı",
+	HereMosqueMessage	: "...",
+	
+	MinutesToIqama		: "Iqama in...",
+	MinutesToDoha		: "Doha in...",
+	AzanCalling			: " Azan ",
+	NextPrayTime	 	: "Sonraki dua",
+	NamesOfPrayings		: "İMSAK,GÜNEŞ,ÖĞLE,İKINDI,AKŞAM,YATSı",
+	NamesWeekDays		: "Pazar,Pazartesi,Salı,Çarşamba,Perşembe,Cuma Cumartesi",
+	NamesMonthsMiladi	: "Ocak,Şubat,Mart,Nisan,Mayıs,Haziran,Temmuz,Ağustos,Eylül,Ekim,Kasım,Aralık ",
+	MenuMonthsHijri		: "Muharrem,Safer,Rebiülevvel,Rebiülâhir,Cemaziyelevvel,Cemaziyelâhir,Recep,Şaban,Ramazan,Şevval,Zilkade,Zilhicce",
+	
+	MenuFullScreen		: "Tam ekran",
+	MenuMosqueNAME		: "Cami Adını Düzenle",
+	MenuMosqueMessage	: "Cami Duyurusunu Düzenle",
+	MenuLocation		: "Cami Konumu",
+	MenuHijriClick		: "Hicri Tarihi değiştir",
+	MenuThemes			: "duvar kağıdını değiştir",
+	MenuOptions			: "Seçenekler",
+	MenuAzkar			: "Azkar Ekranı",
+	MenuSliders			: "Slayt Gösterisi",
+	MenuAdjustments		: "Namaz Vakitlerini Ayarlayın",
+	MenuBlackscreen		: "Siyah Ekranı Ayarla",
+	MenuDimmerDirect	: "Ekranı Manuel Olarak Gizle",
+	MenuMosquelogo		: "Cami Logosu",
+	MenuRestartApp		: "Uygulamayı Yeniden Başlatın",
+	MenuGoMobile		: "Mobil uygulama",
+	MenuAboutTawkit		: "Tawkit uygulaması hakkında",
+
+	XXDoaAzanIqama		: "الدعاء بين الأذان والإقامة لا يرد",
+	XXFastingMonday		: "Hatırlatma : Yarın Pazartesi Oruç Tutmayı Unutmayın",
+	XXFastingThursday	: "Hatırlatma : Yarın Perşembe Oruç Tutmayı Unutmayın",
+	
+	XX_Themes_0			: "Kişisel arka planı kullanmak için resme tıklayın",
+	XX_Themes_1			: "Arka plan resmini değiştirmek için dosyayı değiştirmeniz gerekir",
+	XX_Themes_2			: "klasörde",
+	XX_Themes_3			: "Renkleri değiştirmek için aşağıdaki metne tıklayın",
+	XX_Themes_4			: "Renkleri Düzenle",
+	
+	XX_OPTION_0			: "24 Saati Etkinleştir",
+	XX_OPTION_1			: "Tam saati etkinleştir",
+	XX_OPTION_2			: "Dimm Geçmiş Dualar",
+	
+	XX_ADJUST_0			: "Ezan ve Geri Sayım Arasındaki Zaman",	
+	XX_ADJUST_1			: "Geri Sayımı Göster",
+	XX_ADJUST_2			: "Ezan ve Iqama için Sesi Kullan",
+	XX_ADJUST_3			: "Ramazanda Yatsı'ya 30 Dakika Ekleyin",
+	
+	XX_IQAMA			: "Iqama",
+	XX_DOHA				: "Doha",
+	
+	XX_DIMMER_0			: "Dua ederken Siyah Ekranı Etkinleştir",
+	XX_DIMMER_1			: "Güneşin Doğuşundan 35 Dakika Sonra Siyah Ekran",
+	XX_DIMMER_2			: "Yatsı 'dan 50 Dakika Sonra Siyah Ekran",
+	
+	XX_DIMMER_3			: "Öğleden Önce Siyah Ekran",
+	XX_DIMMER_4			: "Öğleden sonra ekranı göster",
+	XX_DIMMER_5			: "Ekranı Gizlemek için Namaz Vakitleri Süresi",
+	
+	XX_AZKAR_0			: "Dualardan Sonra Azkar Resimlerini Etkinleştir",
+	XX_AZKAR_1			: "Resimler Her 20 Saniyede Bir Gösteriliyor/Gizleniyor",
+	XX_AZKAR_2			: "Azkar Resimleri Klasörde:",
+	XX_AZKAR_3			: "Namazdan Sonra Görünen Azkar Süresini Seçin :",
+	XX_AZKAR_4			: "Sadece Bir Resim Göster, 5 Dakika Sabit",
+	XX_AZKAR_5			: "Azkar'ı Test etmek için (ÖĞLE time)'a tıklayın",
+	XX_AZKAR_6			: "Azkar'ı Durdurmak İçin Azkar Ekranına Tıklayınız.",
+	
+	XX_MOBILE_1			: "Caminiz İçin Bir Mobil Uygulama Oluşturmak İçin Şu Adrese gidin:",
+	
+	XX_SLIDERS_0		: "Kişisel Slaytları Etkinleştir",
+	XX_SLIDERS_1		: "Slaytlar, Azkar'dan sonra ekranda rastgele görüntülenen görüntülerdir.",
+	XX_SLIDERS_2		: "Bir sonraki Namaza kadar her 20 saniyede bir gösterilir/gizlenirler.",
+	XX_SLIDERS_3		: "Kendi Slaytlarınızı eklemek için (Slides) klasörüne en fazla 7 resim yerleştirin.",
+	XX_SLIDERS_4		: "Slaytları test etmek için Öğle Yemeği'ne tıklayın",
+	XX_SLIDERS_5		: "Slaytları durdurmak için Slaytlar Ekranına tıklayın.",
+	
+	XX_TIMES_0			: "Hesaplamayı Kullan",
+	XX_TIMES_1			: "Dosyaları Kullan",
+	XX_TIMES_2			: "Cami Şehrinizi Seçin",
+	XX_TIMES_3			: "Şehriniz listede yoksa (00.000.PERSONAL) öğesini seçin.",
+	XX_TIMES_4			: "Ve (/data/wtimes-00.000.ini) dosyasını cami saatinize göre değiştirin.",
+	XX_TIMES_5			: "Uygulamayı Yeniden Yükle",
+	
+	XX_TIMES_6			: "değiştir",
+	
+	XX_CALC_0			: "Ülke :",
+	XX_CALC_1			: "Şehir :",
+	XX_CALC_2			: "Yöntem :",
+	XX_CALC_3			: "İkindi Hesaplaması :",
+	
+	XX_BOX_0			: "Tarihi ayarlamak için Hicri tarihe tıklayın (+ 2 / -2) ",
+	XX_BOX_1			: "Cami logonuzu ekrana koymak için,",
+	XX_BOX_2			: "Uygulama klasöründeki dosyayı (logo.png) değiştirin.",
+	
+	XX_COMMANDER_0		: "Sesli Komutlar Zamanlayıcısını Etkinleştirin",
+	XX_MISC_0			: "DEĞİŞİKLİKLERDEN SONRA SAYFAYI YENİDEN YÜKLEYİN !",
+	XX_MISC_1			: "Duvar kağıtları arasında geçiş yapmak için İMSAK tıklayın",
+	
+	XX_NO_SELL			: "FREEWARE UYGULAMASI, SATIŞI YASAK",
+	
+	XX_JOMOA			: "CUMA",
+	XX_IQAMAASHOUR		: "Iqama'yı saatle göster",
+	XX_SUMMERTIME		: "Yaz aylarında namaz vakitlerine otomatik olarak 1 saat ekleyin",
+	
+	W9_ARABICDIGITS			: "Enable Arabic Digits",
+	XX_WCSV_MESSAGE			: "Enable own file (wcsv.ini) for praying Times and Iqama",
+	W9_SetJOMOA_TIME		: "Adjust Jumuah time",
+	W9_eIQAMAUNDERNAMES		: "Put Iqama times under the Prayers names",
+	W9_setFridayJomoaTIME	: "Jomoa time of your Mosque. If automatic every Friday, write AUTO",
+
+	
+};
